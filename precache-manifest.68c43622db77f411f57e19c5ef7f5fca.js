39bf941d40cd0ead84fea849d66a92efb3ac587e
Matias <matias.lazos@gmail.com>
2020-06-29 02:11:13 -0400

    Updates

---
+++ precache-manifest.68c43622db77f411f57e19c5ef7f5fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec02e3b32c27f14383386383f9cdd68d",
+    "url": "/mlazos7/crypto-dashboard/index.html"
+  },
+  {
+    "revision": "baa6bbb67ab0e733bdb2",
+    "url": "/mlazos7/crypto-dashboard/static/css/main.74b45f36.chunk.css"
+  },
+  {
+    "revision": "f682ba21ace4a4497563",
+    "url": "/mlazos7/crypto-dashboard/static/js/2.3ef3cedf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mlazos7/crypto-dashboard/static/js/2.3ef3cedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa6bbb67ab0e733bdb2",
+    "url": "/mlazos7/crypto-dashboard/static/js/main.59fce8d4.chunk.js"
+  },
+  {
+    "revision": "754314147a72006861b9",
+    "url": "/mlazos7/crypto-dashboard/static/js/runtime-main.70607f7c.js"
+  }
+]);
