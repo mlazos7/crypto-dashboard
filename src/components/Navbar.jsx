84374bffac29800713e0ab53a5526551f651bd23
Matias <matias.lazos@gmail.com>
2020-10-13 00:14:54 -0300

    se agrega el classTheme al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,11 @@ import LightThemeIcon from "../assets/l-theme.png";
 import DarkThemeIcon from "../assets/d-theme.png"
 import { useDispatch, useSelector } from "react-redux";
 import { changeTheme } from "../redux/actions";
+import { classTheme} from "../Theme";
 
 export default function Navbar(props) {
     const dispatch = useDispatch();
     const theme = useSelector( (state) => state.crypto.theme)
-    const classThemePrimary = `primary-${theme ? 'dark' : 'light'}`
-    const classThemeSecondary = `secondary-${theme ? 'dark' : 'light'}`
 
     const handleClick = (e) => {
         e.preventDefault();
@@ -19,12 +18,12 @@ export default function Navbar(props) {
   return (
     <div className="navbar">
       <div className="navbar-title">
-        <h1 className={classThemePrimary}>Cryptocurencies Dashboard</h1>
-        <p className={classThemeSecondary}>Total Market Cap: {formatterUSD.format(props.totalMarketCap)}</p>
+        <h1 className={classTheme.Primary(theme)}>Cryptocurencies Dashboard</h1>
+        <p className={classTheme.Secondary(theme)}>Total Market Cap: {formatterUSD.format(props.totalMarketCap)}</p>
       </div>
       <div className="navbar-theme">
         <div className="display-theme">
-          <p className={classThemeSecondary}>Dark Mode</p>
+          <p className={classTheme.Secondary(theme)}>Dark Mode</p>
           <img src={theme ? DarkThemeIcon : LightThemeIcon} onClick={handleClick} alt=""></img>
         </div>
       </div>
